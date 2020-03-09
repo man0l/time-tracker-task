9a30575134d9a8c3a4a6fa8fd1a99de9729c9aec
Manol Trendafilov <m.trendafilov@propy.com>
2020-03-09 17:33:15 +0200

    add validations for user

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,19 +4,30 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\RadioType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('password')
+            ->add('email', InputType::class, [
+                'constraints' => [
+                    new NotBlank(),
+                    new UniqueEntity()
+                ]
+            ])
+            ->add('password', InputType::class, [
+                'constraints' => [
+                    new NotBlank()
+                ]
+            ])
         ;
     }
 
