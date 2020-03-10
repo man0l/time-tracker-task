eac2c9853a1c31bb249174397cf32244a0efdd6b
Manol Trendafilov <m.trendafilov@propy.com>
2020-03-10 10:14:44 +0200

    create payment rate entity and repo, migration for payment

---
+++ src/Entity/Payment.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PaymentRepository")
+ */
+class Payment
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $weekday;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $hours;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $rate;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getWeekday(): ?bool
+    {
+        return $this->weekday;
+    }
+
+    public function setWeekday(bool $weekday): self
+    {
+        $this->weekday = $weekday;
+
+        return $this;
+    }
+
+    public function getHours(): ?int
+    {
+        return $this->hours;
+    }
+
+    public function setHours(int $hours): self
+    {
+        $this->hours = $hours;
+
+        return $this;
+    }
+
+    public function getRate(): ?float
+    {
+        return $this->rate;
+    }
+
+    public function setRate(float $rate): self
+    {
+        $this->rate = $rate;
+
+        return $this;
+    }
+}
