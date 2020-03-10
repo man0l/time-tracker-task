5dfbe9ea8eddd23b1aa7b32e214cf6c5b16a54cb
Manol Trendafilov <m.trendafilov@propy.com>
2020-03-10 11:24:30 +0200

    style form, add validations

--- src/Form/RateType.php
+++ src/Form/RateType.php
@@ -3,17 +3,31 @@
 namespace App\Form;
 
 use App\Entity\Rate;
+use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\PositiveOrZero;
 
 class RateType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('from_hours')
-            ->add('to_hours')
+            ->add('from_hours', NumberType::class, ['constraints' => [
+                new NotBlank(),
+                new PositiveOrZero()
+            ]])
+            ->add('to_hours',NumberType::class, ['constraints' => [
+                new NotBlank(),
+                new PositiveOrZero()
+            ]])
+            ->add('rate', NumberType::class, ['constraints' => [
+                new NotBlank(),
+                new PositiveOrZero()
+            ]])
             ->add('weekday')
         ;
     }
